c91ea32aee1a87ee8c55d2c0fcfe25381912d3a5
Nicolas Rouleau <nicolas@digitalmilitia.net>
2021-08-31 15:48:03 -0400

    Mise à jour de la méthode d'ajout d'usager

--- modeles/Usager.class.php
+++ modeles/Usager.class.php
@@ -11,84 +11,83 @@ namespace TodoWS\modeles;
  * 
  */
 class Usager extends Modele {	
-	
-		
-	/**
-	 * Retourne la liste des usager
-	 * @access public
-	 * @return Array
-	 */
-	public function getListe() 
-	{
-		$res = Array();
-		if($mrResultat = $this->_db->query("select * from usager"))
-		{
-			while($usager = $mrResultat->fetch_assoc())
-			{
-				$res[] = $usager;
-			}
-		}
-		return $res;
-	}
-	
-	/**
-	 * Ajoute un usager
-	 * @access public
-	 * @param String $courriel Courriel de l'usager
-	 * @return int Identifiant de l'usager
-	 */
-	public function ajouterUsager($courriel) 
-	{
-		$id_usager = 0;
-		$usager = $this->getUsagerParCourriel($courriel);
-		if(!$usager)
-		{
-			$query = "INSERT INTO usager (courriel) 
-			VALUES ('".$courriel. "')";
-			$resQuery = $this->_db->query($query);
-			$id_usager = ($this->_db->insert_id ? $this->_db->insert_id : 0);		
-		}
-		else {
-			$id_usager = $usager['id_usager']; 
-		}
-		
-		return $id_usager;
-	}
-	
-	
-	
-	/**
-	 * Récupère un usager par id
-	 * @access public
-	 * @param int $id Identifiant de l'usager
-	 * @return Array
-	 */
-	public function getUsagerParId($id) 
-	{
-		$res = Array();
-		if($mrResultat = $this->_db->query("select * from usager where id_usager=". $id))
-		{
-			$usager = $mrResultat->fetch_assoc();
-		}
-		return $usager;
-	}
-	
-	/**
-	 * Récupère un usager par Courriel
-	 * @access public
-	 * @param String $courriel Courriel de l'usager
-	 * @return Array
-	 */
-	public function getUsagerParCourriel($courriel) 
-	{
-		$res = Array();
-		
-		if($mrResultat = $this->_db->query("select * from usager where courriel='". $courriel."'"))
-		{
-			$usager = $mrResultat->fetch_assoc();
-		}
-		return $usager;
-	}
-	
-	
+    
+        
+    /**
+     * Retourne la liste des usager
+     * @access public
+     * @return Array
+     */
+    public function getListe() 
+    {
+        $res = Array();
+        if($mrResultat = $this->_db->query("select * from usager"))
+        {
+            while($usager = $mrResultat->fetch_assoc())
+            {
+                $res[] = $usager;
+            }
+        }
+        return $res;
+    }
+    
+    /**
+     * Ajoute un usager
+     * @access public
+     * @param String $courriel Courriel de l'usager
+     * @return int Identifiant de l'usager
+     */
+    public function ajouterUsager($courriel) 
+    {
+        $id_usager = 0;
+        
+        if($usager = $this->getUsagerParCourriel($courriel))
+        {
+            return $usager['id_usager'];                    
+        }
+
+        $query = "INSERT INTO usager (courriel) 
+                VALUES ('".$courriel. "')";
+            
+        $resQuery = $this->_db->query($query);
+
+        return $this->_db->insert_id ?? 0;
+    }
+    
+    
+    
+    /**
+     * Récupère un usager par id
+     * @access public
+     * @param int $id Identifiant de l'usager
+     * @return Array
+     */
+    public function getUsagerParId($id) 
+    {
+        $res = Array();
+        if($mrResultat = $this->_db->query("select * from usager where id_usager=". $id))
+        {
+            $usager = $mrResultat->fetch_assoc();
+        }
+        return $usager;
+    }
+    
+    /**
+     * Récupère un usager par Courriel
+     * @access public
+     * @param String $courriel Courriel de l'usager
+     * @return Array
+     */
+    public function getUsagerParCourriel($courriel) 
+    {
+        $res = Array();
+        
+        if($mrResultat = $this->_db->query("select * from usager where courriel='". $courriel."'"))
+        {
+            $usager = $mrResultat->fetch_assoc();
+        }
+        return $usager;
+    }
+    
+    
 }
